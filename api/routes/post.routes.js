c8cb8a43851cf372b98c8a0737949b481914c8a0
mahesh <jadhao.mahesh1009@gmail.com>
2024-02-12 19:15:54 +0530

    update post functionality

--- api/routes/post.routes.js
+++ api/routes/post.routes.js
@@ -1,10 +1,11 @@
 import expres from 'express';
 import {verifyToken} from '../utils/verifyUser.js'
-import { create ,getposts ,deletepost} from '../controllers/post.controllers.js';
+import { create ,getposts ,deletepost ,updatepost} from '../controllers/post.controllers.js';
 
 const router = expres.Router();
 
 router.post('/create',verifyToken,create);
 router.get("/getPosts",getposts)
 router.delete('/deletepost/:postId/:userId', verifyToken, deletepost)
+router.put('/updatepost/:postId/:userId', verifyToken, updatepost)
 export default router;
