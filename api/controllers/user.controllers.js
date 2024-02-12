d24101c282725216006a8f62f4500a4a12de5b5f
mahesh <jadhao.mahesh1009@gmail.com>
2024-02-12 20:04:50 +0530

    create get user api route

--- api/controllers/user.controllers.js
+++ api/controllers/user.controllers.js
@@ -169,3 +169,50 @@ export const updateUser = async (req, res, next) => {
     next(error)
    }
  }
+
+ export const  getUsers= async(req,res,next)=>{
+
+  if(!req.user.isAdmin){
+    return next(errorHander(403,"You are not allowed to see all users"))
+  }
+
+try {
+    const startIndex =parseInt(req.body.startIndex) || 0 ;
+    const limit = parseInt(req.query.limit) || 9 ;
+    const sortDirection = req.query.sort === "asc"? 1 : -1 ;
+  
+    const users = await User.find()
+      .sort({createdAt :sortDirection})
+      .skip(startIndex)
+      .limit(limit);
+  
+      const usersWithoutPassword = users.map((user)=>{
+        const {password ,...rest} = user._doc;
+        return rest;
+      });
+     
+
+      const totalUsers = await User.countDocuments();
+       const now = new Date();
+       const oneMonthAgo =new Date(
+        now.getFullYear(),
+        now.getMonth()-1,
+        now.getDate()
+       )
+
+        const lastMonthUser = await User.countDocuments({
+          createdAt:{$gt:oneMonthAgo}
+        })
+
+        res.status(200).json({
+          users:usersWithoutPassword,
+          totalUsers,
+          lastMonthUser,
+        });
+
+
+} catch (error) {
+  next(error);
+}
+
+ }
