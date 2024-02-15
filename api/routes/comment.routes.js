18eec443449b929ee942955bc280a5130ecc3526
mahesh <jadhao.mahesh1009@gmail.com>
2024-02-15 11:49:20 +0530

    like delete edit functionality to comment

--- api/routes/comment.routes.js
+++ api/routes/comment.routes.js
@@ -1,11 +1,21 @@
 import express from 'express';
-import { createComment ,getPostComments } from '../controllers/comments.controllers.js';
 import { verifyToken } from '../utils/verifyUser.js';
-
+import {
+  createComment,
+  deleteComment,
+  editComment,
+  getPostComments,
+  getcomments,
+  likeComment,
+} from '../controllers/comments.controllers.js';
 
 const router = express.Router();
 
-router.post('/create',verifyToken,createComment);
-router.get('/getPostComments/:postId',getPostComments)
+router.post('/create', verifyToken, createComment);
+router.get('/getPostComments/:postId', getPostComments);
+router.put('/likeComment/:commentId', verifyToken, likeComment);
+router.put('/editComment/:commentId', verifyToken, editComment);
+router.delete('/deleteComment/:commentId', verifyToken, deleteComment);
+router.get('/getcomments', verifyToken, getcomments);
 
 export default router;
