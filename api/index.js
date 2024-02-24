19756883d8b91211d1c3a401ed22957c6b19d1c0
mahesh <jadhao.mahesh1009@gmail.com>
2024-02-24 14:07:47 +0530

    c

--- api/index.js
+++ api/index.js
@@ -1,51 +1,52 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRoutes from './routes/user.routes.js'
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+import postRoutes from './routes/post.route.js';
+import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
-import authRoutes from './routes/auth.routes.js'
-import postRoute from './routes/post.routes.js'
-import commentRoutes from './routes/comment.routes.js'
 import path from 'path';
 
 dotenv.config();
-mongoose.connect(process.env.MONGO).then(()=>{
-    console.log("MongoDb is connected")
-}).catch((err)=>{
-    console.log(err)
-})
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('MongoDb is connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const __dirname = path.resolve();
 
-
 const app = express();
-app.use(express.json())
-app.listen(3000,()=>{
-    console.log("app is running in port 3000")
-})
-app.use(cookieParser())
 
-app.use("/api/user",userRoutes)
-app.use("/api/auth",authRoutes )
-app.use('/api/post',postRoute)
-app.use('/api/comment',commentRoutes)
+app.use(express.json());
+app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname,'/client/dist')));
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
+});
 
-app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname,'client','dist','index.html'))
-})
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/post', postRoutes);
+app.use('/api/comment', commentRoutes);
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
-app.use((err,req,res,next)=>{
-    const statusCode =err.statusCode||500;
-    const message = err.message || 'Internal Server Error';
-    res.status(statusCode).json({
-        success:false,
-        statusCode,
-        message,
-    })
-   
-})
-    
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
