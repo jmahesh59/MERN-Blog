4ab6a398cb111b868390ae5dadffc5934fdd4c83
mahesh <jadhao.mahesh1009@gmail.com>
2024-02-24 12:10:13 +0530

    complete about and project pages

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import CallToAction from '../components/CallToAction'
 
 function Projects() {
   return (
-    <div>
-      project
+    <div className='min-h-screen max-w-2xl flex mx-auto justify-center items-center flex-col gap-3 p-3  '>
+      <h1 className='text-3xl font-semibold'>Projects</h1>
+      <p className='text-md text-gray-500 '>Build fun and engaging projects while learning HTML, CSS, and JavaScript!
+      </p>
+      <CallToAction/>
     </div>
   )
 }
